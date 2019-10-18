540ce6e74bf3b7f7c2068045edb5bb39f84950fe
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2019-10-18 01:14:22 +0100

    v1 com menus

--- client.java
+++ client.java
@@ -1,11 +1,9 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
 public class Client {
 
     public static void main(String[] args) {
-        GUI userGUI = new GUI();
+        // GUI userGUI = new GUI();
+        UI userUI = new UI("admin");
+        userUI.mainMenu();
     }
 
 }
