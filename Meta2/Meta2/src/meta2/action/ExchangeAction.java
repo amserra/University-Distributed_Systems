2b6987d51ebbcb55f7b614e3412545faee9dc80a
JGMF <jgmfernandes14@hotmail.com>
2019-12-10 16:26:13 +0000

    teste

---
+++ Meta2/Meta2/src/meta2/action/ExchangeAction.java
@@ -0,0 +1,43 @@
+package meta2.action;
+
+import com.github.scribejava.apis.FacebookApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.oauth.OAuth20Service;
+import com.opensymphony.xwork2.ActionSupport;
+import meta2.model.HeyBean;
+import org.apache.struts2.interceptor.SessionAware;
+import rmiserver.ServerInterface;
+
+import java.util.Map;
+
+
+public class ExchangeAction extends ActionSupport implements SessionAware {
+    private static final long serialVersionUID = 4L;
+    private Map<String, Object> session;
+    private String myHiddenField;
+
+    @Override
+    public String execute() {
+
+        System.out.println("Let's trade code for token!");
+        System.out.println(myHiddenField);
+        return SUCCESS;
+
+    }
+
+    public HeyBean getHeyBean() {
+        if(!session.containsKey("heyBean"))
+            this.setHeyBean(new HeyBean());
+        return (HeyBean) session.get("heyBean");
+    }
+
+    public void setHeyBean(HeyBean heyBean) {
+        this.session.put("heyBean", heyBean);
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+}
+
